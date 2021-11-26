5d036f673721450888c45ab02ec166bdad9ccb06
Djordje Lukic <djordje.lukic@docker.com>
2021-11-26 16:02:14 +0100

    Open the image passed as an argument
    Signed-off-by: Djordje Lukic <djordje.lukic@docker.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# qoi image viewer
+
+A simple image viewer for [qoi](https://phoboslab.org/log/2021/11/qoi-fast-lossless-image-compression) images.
+
+Thanks to [@ChevyRay](https://twitter.com/ChevyRay) for the heavy lifting of porting this to rust.
+
+Thanks to [@phoboslab](https://twitter.com/phoboslab) for the fun.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::{fs::File, io::BufWriter, num::NonZeroUsize, time::Duration};
+use std::env;
 
-// use image::GenericImageView;
 use qoi::Pixel;
 use sdl2::{
     event::Event,
@@ -10,31 +9,17 @@ use sdl2::{
 };
 
 fn main() {
-    // let img = image::open("cat.jpg").unwrap();
-
-    // let w = img.width();
-    // let h = img.height();
-    // let pixels = img.pixels();
-    // let file = File::create("cat.qoi").unwrap();
-
-    // let p = pixels.map(|(_, _, p)| Pixel::rgba(p.0[0], p.0[1], p.0[2], p.0[3]));
-
-    // qoi::encode(
-    //     NonZeroUsize::new(w as usize).unwrap(),
-    //     NonZeroUsize::new(h as usize).unwrap(),
-    //     p,
-    //     BufWriter::new(file),
-    // )
-    // .unwrap();
-
+    let args: Vec<String> = env::args().collect();
+    let image = &args[1];
     let mut pxls: Vec<Pixel> = Vec::new();
-    let (width, height) = qoi::decode_file_into_vec("cat.qoi", &mut pxls).unwrap();
+
+    let (width, height) = qoi::decode_file_into_vec(image, &mut pxls).unwrap();
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", width as u32, height as u32)
+        .window("QOI", width as u32, height as u32)
         .position_centered()
         .build()
         .unwrap();
@@ -47,38 +32,41 @@ fn main() {
 
     let texture_creator = canvas.texture_creator();
     let mut texture = texture_creator
-        .create_texture(PixelFormatEnum::RGBA8888, TextureAccess::Streaming, w, h)
+        .create_texture(
+            PixelFormatEnum::RGBA8888,
+            TextureAccess::Streaming,
+            width as u32,
+            height as u32,
+        )
         .unwrap();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.clear();
+
+    texture.with_lock(None, |a, _| cc(a, &pxls)).unwrap();
+    canvas.copy(&texture, None, None).unwrap();
+    canvas.present();
+
     'running: loop {
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
-                _ => {}
-            }
+        let event = event_pump.wait_event();
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => break 'running,
+            _ => {}
         }
-
-        texture.with_lock(None, |a, _| cc(a, &pxls)).unwrap();
-        canvas.copy(&texture, None, None).unwrap();
-        canvas.present();
-
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
 
-fn cc(a: &mut [u8], pxls: &Vec<Pixel>) {
+fn cc(a: &mut [u8], pxls: &[Pixel]) {
     let u128_slice: &mut [u32] = bytemuck::cast_slice_mut(a);
 
     for (i, l) in u128_slice.iter_mut().enumerate() {
         *l = (pxls[i].r as u32) << 24
             | (pxls[i].g as u32) << 16
             | (pxls[i].b as u32) << 8
-            | (pxls[i].a as u32) << 0;
+            | (pxls[i].a as u32);
     }
 }
